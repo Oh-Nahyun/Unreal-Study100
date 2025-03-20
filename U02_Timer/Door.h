aa0ff9c8a68a2832be2e9ebaf36d90abc2160455
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-03-20 11:51:26 +0900

    Update Door.h

--- U02_Timer/Door.h
+++ U02_Timer/Door.h
@@ -1,1 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Door.generated.h"
+
+UCLASS()
+class U02_TIMER_API ADoor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADoor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* DoorMesh;
+
+	UPROPERTY(EditAnywhere)
+	float CloseTime;
+
+	float DoorDeltaTime;
+
+	bool bOpen;
+
+	FTimerHandle DoorTimerHandle;
+
+	FRotator OrientRotation;
+
+public:
+	void Open();
+
+	void Close();
+
+};
 
