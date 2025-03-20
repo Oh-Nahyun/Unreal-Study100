04c3b1dc8b0be89b2ac6e06da81ebfcb069b53b7
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-03-20 11:50:34 +0900

    Update Countdown.h

--- U02_Timer/Countdown.h
+++ U02_Timer/Countdown.h
@@ -1,1 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Components/TextRenderComponent.h"
+//#include "Runtime/Engine/Classes/Components/TextRenderComponents.h"
+
+#include "Countdown.generated.h"
+
+UCLASS()
+class U02_TIMER_API ACountdown : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACountdown();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	//// Called every frame
+	//virtual void Tick(float DeltaTime) override;
+
+public:
+	///// Timer Number
+	UPROPERTY(EditAnywhere)
+
+	int32 CountdownTime;
+
+	UTextRenderComponent* CountdownText;
+
+public:
+	///// 화면에 글자 출력하는 함수
+	void UpdateTimerDisplay();
+
+	///// 타이머 관련 함수
+	void AdvencedTimer();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void CountdownHasFinished();
+	virtual void CountdownHasFinished_Implementation();
+
+	FTimerHandle CountdownTimerHandle;
+};
 
