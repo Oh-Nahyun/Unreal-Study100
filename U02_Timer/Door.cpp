bcb2603af03a411aa9e36090861929b090943346
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-03-20 11:51:43 +0900

    Update Door.cpp

--- U02_Timer/Door.cpp
+++ U02_Timer/Door.cpp
@@ -1,1 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Door.h"
+
+// Sets default values
+ADoor::ADoor()	///// 생성자
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));	///// DoorMesh 생성하기
+	DoorMesh->SetupAttachment(RootComponent);									///// Root 에 붙이기
+
+	CloseTime = 3.0f;
+}
+
+// Called when the game starts or when spawned
+void ADoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	OrientRotation = GetActorRotation();	///// 현재의 값 저장하기
+
+	GetWorldTimerManager().SetTimer(DoorTimerHandle, this, &ADoor::Open, 0.03f, true);
+}
+
+// Called every frame
+void ADoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	DoorDeltaTime += DeltaTime;	///// 누적 Time
+}
+
+void ADoor::Open()
+{
+	if (!bOpen)
+	{
+		bOpen = true;
+		DoorDeltaTime = 0.0f;
+	}
+	
+	FRotator rotation = GetActorRotation();
+	rotation = OrientRotation + FRotator(0.0f, FMath::Lerp(0.0f, 90.0f, DoorDeltaTime), 0.0f);
+	SetActorRotation(rotation);
+
+	if (DoorDeltaTime > 1.0f)
+	{
+		GetWorldTimerManager().ClearTimer(DoorTimerHandle);
+		GetWorldTimerManager().SetTimer(DoorTimerHandle, this, &ADoor::Close, 0.03f, true, CloseTime);
+	}
+}
+
+void ADoor::Close()
+{
+	if (bOpen)
+	{
+		bOpen = false;
+		DoorDeltaTime = 0.0f;
+	}
+
+	FRotator rotation = GetActorRotation();
+	rotation = OrientRotation + FRotator(0.0f, FMath::Lerp(90.0f, 0.0f, DoorDeltaTime), 0.0f);
+	SetActorRotation(rotation);
+
+	if (DoorDeltaTime > 1.0f)
+	{
+		GetWorldTimerManager().ClearTimer(DoorTimerHandle);
+	}
+}
 
