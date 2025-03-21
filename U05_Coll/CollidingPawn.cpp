3005eca6740b178099bc9b900d5896afbf073a01
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-03-21 21:48:54 +0900

    Create CollidingPawn.cpp

---
+++ U05_Coll/CollidingPawn.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CollidingPawn.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Components/SphereComponent.h"
+#include "UObject/ConstructorHelpers.h"
+
+// Sets default values
+ACollidingPawn::ACollidingPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 물리에 반응하는 구체 (루트 컴포넌트)
+	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
+	RootComponent = SphereComponent;
+	SphereComponent->InitSphereRadius(40.0f);
+	SphereComponent->SetCollisionProfileName(TEXT("Pawn"));
+
+	// 구체 메시 컴포넌트 생성 후 배치
+	UStaticMeshComponent* SphereVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualRepresentation"));
+	SphereVisual->SetupAttachment(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	if (SphereVisualAsset.Succeeded())
+	{
+		SphereVisual->SetStaticMesh(SphereVisualAsset.Object);
+		SphereVisual->SetRelativeLocation(FVector(0.0f, 0.0f, -40.0f));
+		SphereVisual->SetWorldScale3D(FVector(0.8f));
+	}
+
+	// 파티클 시스템 생성 (활성화 / 비활성화)
+	OurPartivleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MovementParticle"));
+	OurPartivleSystem->SetupAttachment(SphereVisual);
+	OurPartivleSystem->bAutoActivate = false;
+	OurPartivleSystem->SetRelativeLocation(FVector(-20.0f, 0.0f, 20.0f));
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire"));
+	if (ParticleAsset.Succeeded())
+	{
+		OurPartivleSystem->SetTemplate(ParticleAsset.Object);
+	}
+
+	// 스프링 암
+
+	// 카메라
+
+	// 컨트롤 (빙의)
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+// Called when the game starts or when spawned
+void ACollidingPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACollidingPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACollidingPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
